8236f757d7d4986926088badc1061f1110efe773
Alex Kramer <kramer612@gmail.com>
2018-11-09 17:50:15 -0500

    starting process html & js

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var words = [
+    "beep",
+    "flute",
+    "empire",
+    "serpent",
+    "trench"
+];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+var blanks = [];
+for(i=0; i<words.length; i++) {
+    blanks[i] = "_";
+}
+
+var remaining = word.length;
+
+while (remaining > 0) {
+    
+}
