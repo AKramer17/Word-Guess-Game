26f5dfccecd46a9d5326d4677fc82eedc7e83de3
Alex Kramer <kramer612@gmail.com>
2018-11-10 22:03:16 -0500

    final attempt

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,8 @@
+
+
+
+$(document).ready(function(){
+
 var words = [
     "beep",
     "flute",
@@ -7,14 +12,45 @@ var words = [
 ];
 
 var word = words[Math.floor(Math.random() * words.length)];
+console.log(word);
 
-var blanks = [];
-for(i=0; i<words.length; i++) {
-    blanks[i] = "_";
+function hiddenLetters() {
+    var answer = [];
+for(i=0; i<word.length; i++) {
+    answer[i] = "_";
+    console.log(answer);
+}
+//return answer;
+
+for(j=0; j < word.length; j++) {
+    if (word[j] == userInput) {
+        answer[j] == word[j];
+    }
+}
 }
 
-var remaining = word.length;
+var remainingLetters = word.length;
+var remainingGuesses = 12;
 
-while (remaining > 0) {
-    
+while (remainingLetters > 0 && remainingGuesses > 0) {
+    answer.join(" ");
 }
+
+$("input").on("keypress", function() {
+    //if key pressed = words[i][j], change _ to letter pressed.
+    if (keypress == )
+});
+for(var i=0; remainingGuesses > 0; i++) {
+
+}
+
+var i = 12;  //possible conflict with previous for loop?
+
+// guesses
+while (i>0) {
+$("input").keypress(function(){
+    $("span").text(i -= 1);
+    });
+}
+    var userInput = $("#input").on("keypress");
+});
